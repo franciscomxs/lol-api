5974dda0d29e1234965305bacbd9ea62f9eb24e9
Francisco Martins <franciscomxs@gmail.com>
2016-07-21 15:00:33 -0300

    Fix spec file name

--- spec/lol/api/endpoints/summoner_spec.rb
+++ spec/lol/api/endpoints/summoner_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe LOL::Api::Endpoints::Summoner do
   let(:region) { 'BR1' }
-  let(:summoner_id) { '4691228' }
+  let(:summoner_ids) { '4691228' }
 
   describe '.call' do
     before do
@@ -11,12 +11,12 @@ describe LOL::Api::Endpoints::Summoner do
 
     it 'is a alias for #data' do
       expect_any_instance_of(LOL::Api::Endpoints::Summoner).to receive(:data)
-      LOL::Api::Endpoints::Summoner.(region: region, summoner_id: summoner_id)
+      LOL::Api::Endpoints::Summoner.(region: region, summoner_ids: summoner_ids)
     end
   end
 
   describe '#data' do
-    let(:response) { LOL::Api::Endpoints::Summoner.new(region: region, summoner_id: summoner_id).data }
+    let(:response) { LOL::Api::Endpoints::Summoner.new(region: region, summoner_ids: summoner_ids).data }
     it { expect(response.status).to eq(200) }
     it { expect(response.body).to be_a(Hash) }
   end
